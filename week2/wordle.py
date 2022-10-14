09498b97dc4f013e7dfb2edc372c49b79726b7cf
Peter122103 <peter19991221@gmail.com>
2022-10-14 17:42:09 +0800

    compare user input and answer

--- week2/wordle.py
+++ week2/wordle.py
@@ -1,3 +1,15 @@
 if __name__ == "__main__":
     # user input
     user_input = input
+
+    # specify an answer
+    answer = "apple"
+
+    #compare user input and answer
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
